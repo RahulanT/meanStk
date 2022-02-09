14aff99e66a0408071bc213cbf422534f215e8b0
RahulanT <62266488+RahulanT@users.noreply.github.com>
2022-02-09 11:22:59 -0500

    Update connection.js
    changed connection env

--- server/connections/connection.js
+++ server/connections/connection.js
@@ -1,14 +1,12 @@
+require('dotenv').config();
 
 const mongoose = require("mongoose");
 // var MongoClient = require('mongodb').MongoClient;
 
-// Replace this with your MONGOURI.
-const MONGOURI = "mongodb+srv://rahulanUser:rahulanpassword@cluster0.jl1my.mongodb.net/authDB?retryWrites=true&w=majority";
-
 const StartMongoServer =  () => {
 
   
-     mongoose.connect(MONGOURI , {
+     mongoose.connect(process.env.MONGO_URI , {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
